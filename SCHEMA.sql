3d25610d0a389cb229c199d1c8f1ab92993788d8
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-09-05 20:39:04 +0100

    Wrote Schema

---
+++ SCHEMA.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_tracker;
+CREATE database employee_tracker;
+USE employee_tracker;
+
+CREATE table employees (
+    id INT NOT NULL,
+    first name VARCHAR (30) NOT NULL,
+    last name VARCHAR (30) NOT NULL,
+    role id INT NOT NULL,
+    manager id INT,
+    PROMARY KEY (id)
+);
+
+CREATE TABLE roles (
+id INT NOT NULL AUTO_INCREMENT,
+title VARCHAR (30) NOT NULL,
+salary DECIMAL (7, 2) NOT NULL,
+department_id INT NOT NULL, 
+
+);
+
+CREATE table departments (
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR (30) NOT NULL,
+PRIMARY KEY (id)
+);
+
+
+
+INSERT INTO employees (id, first name, last name, role id, manager id)
+VALUES (1, " ", "", " ");
