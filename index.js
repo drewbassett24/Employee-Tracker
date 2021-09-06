cfbd6e319741c1ac8953013dd017d21a5c11ceea
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-09-06 10:21:28 +0100

    created a table to show info by surname

--- index.js
+++ index.js
@@ -116,7 +116,16 @@ const mainmenu = () => {
 
 
 // create a table that shows info, by surname
-
+const viewEmployees = () => {
+    const query =
+        'select employees.id AS "Employee Id", CONCAT(employees.first_name, " ", employees.last_name) AS "Full Name", roles.title AS "Role", roles.salary AS "Salary", departments.name as "Department", CONCAT(managers.first_name, " ", managers.last_name) AS "Manager" from employees join roles on employees.role_id + roles.id join departments on roles.department_id + departments.id join employees managers ON employees.manager_id = managers.id order by employees.last_name ASC';
+ 
+    CONNECTION.QUERY(QUERY, (ERR, RES) => {
+        if (err) throw err;
+        console.table(res)
+        mainMenu();
+    });
+};
 
 // create a table of employees by role
 
