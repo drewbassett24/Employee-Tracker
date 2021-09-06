b1109f33f187acc3596856b73383133d7c2e1e25
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-09-06 08:51:00 +0100

    Initial prompts and main menu written

--- index.js
+++ index.js
@@ -19,7 +19,100 @@ connection.connect((err) => {
 });
 
 // prompt user input
+const mainmenu = () => {
+    inquirer
+        .prompt({
+            name: 'action',
+            pagesize: 20,
+            type: 'list',
+            message: 'What would you like to do?',
+            choices: [
+                'View all employees',
+                'View all employees by role',
+                'View all employees by department',
+                'View all employees by manager',
+                new inquirer,Separator(),
+                'Add employee',
+                'Remove employee',
+                'Update employee role',
+                'Update employee manager',
+                new inquirer.Separator(),
+                'Add department',
+                'Update department',
+                'Remove department',
+                new inquirer.Separator(),
+                'Add role',
+                'Remove role',
+                new inquirer.Separator(),
+                'Department Budget',
+                'Exit'
+            ],
+        })
+        .then ((answer) => {
+            switch (answer.action) {
+                case 'View all employees' :
+                    viewEmployees();
+                    break;
 
+                case 'View all employees by role':
+                    viewEmployeeRoles();
+                    break;
+                    
+                case 'View all employees by department':
+                    viewEmployeeDepartment();
+                    break;
+
+                case 'View all employees by manager':
+                    viewEmployeeManagers();
+                    break;
+
+                case 'Add employee':
+                    addEmployee();
+                    break;
+
+                case 'Add role':
+                    addRole();
+                    break;
+
+                case 'Add department':
+                    addDepartment();
+                    break;
+
+                case 'Update employee role':
+                    updateEmployeeRole();
+                    break;
+
+                case 'Update employee manager':
+                    updateEmployeeManager();
+                    break;
+
+                case 'Remove employee':
+                    removeEmployee();
+                    break;
+
+                case 'Remove role':
+                    removeRole();
+                    break;
+
+                case 'Remove department':
+                    removeDepartment();
+                    break;
+
+                case 'Department Budget':
+                    showDepartmentBudget();
+                    break;
+
+                case 'Exit':
+                    connection.end();
+                    break;
+                
+                default:
+                    console.log(`Invalid action: ${answer.action}`);
+                    break;
+
+            }
+        });
+};
 
 
 // create a table that shows info, by surname
